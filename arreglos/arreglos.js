847581fe0d307ccadb09fc59a1f5b8e543d85732
Daniel Garcia <danvi_@msn.com>
2021-10-23 12:49:24 -0500

    arreglos

---
+++ arreglos/arreglos.js
@@ -0,0 +1,36 @@
+const valores = [1, 2, 3, 4, 5];
+
+const resultado = valores.reduce((acc, elem) => (acc = acc + elem));
+
+console.log(resultado);
+
+// const resultadoFilter = valores.filter((nombre) => nombre === 'Juan');
+// const resultadoFind = valores.find((nombre) => nombre === 'Juan');
+// console.log(resultadoFilter);
+// console.log(resultadoFind);
+
+// console.log(resultado);
+
+// const nuevosValores = nombres.map((nom) => nom * 21);
+
+// console.log(nuevosValores);
+
+// arreglo.push('elemento1');
+
+// for (let indice = 0; indice < arreglo.length; indice++) {
+//   console.log(arreglo[indice]);
+// }
+
+// arreglo.forEach(function (valores) {
+//     console.log(valores)
+// })
+
+// arreglo.forEach((valores) => {
+//     console.log(valores)
+//     console.log(valores)
+//     console.log(valores)
+//     console.log(valores)
+
+// })
+
+// arreglo.forEach((valores) => console.log(valores));
