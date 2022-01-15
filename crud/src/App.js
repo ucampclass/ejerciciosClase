9bbd16df1e870d531425952623e884c3de487578
Daniel Garcia <danvi_@msn.com>
2022-01-15 12:38:14 -0600

    chore: add form

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,9 +1,12 @@
 import { useEffect, useState } from 'react';
-import { collection, getDocs } from 'firebase/firestore/lite';
+import { collection, getDocs, addDoc } from 'firebase/firestore/lite';
 import { dbConfig } from './config/firebase';
 
 function App() {
   const [listadoTareas, setListaTareas] = useState([]);
+  // const [form, setForm] = useState({ nombre: '', nota: '' });
+  const [nombre, setNombre] = useState('');
+  const [nota, setNota] = useState('');
 
   useEffect(() => {
     const obtenerDatos = async () => {
@@ -24,6 +27,40 @@ function App() {
     obtenerDatos();
   }, []);
 
+  const submit = (e) => {
+    e.preventDefault();
+    guardaTarea();
+  };
+
+  const guardaTarea = async () => {
+    const datoAGuardar = {
+      nombre,
+      nota,
+    };
+    // const datoAGuardar = {
+    //   nombre:nombre,
+    //   nota: nota,
+    // };
+
+    console.log(datoAGuardar);
+    try {
+      const DB = collection(dbConfig, 'tareas');
+      const item = await addDoc(DB, datoAGuardar);
+      setListaTareas([...listadoTareas, { id: item.id, nombre, nota }]);
+    } catch (e) {
+      console.log('hubo un error');
+      console.log(e);
+    }
+  };
+
+  const handleFormNombre = (texto) => {
+    setNombre(texto);
+  };
+
+  const handleFormNota = (texto) => {
+    setNota(texto);
+  };
+
   return (
     <div className='container'>
       <div className='row'>
@@ -42,13 +79,23 @@ function App() {
         </div>
         <div className='col-6'>
           <h1>Agregar tarea</h1>
-          <form>
+          <form onSubmit={submit}>
             <label className='form-label'>Nombre tarea</label>
-            <input type='text' className='form-control' />
+            <input
+              type='text'
+              className='form-control'
+              onChange={(e) => handleFormNombre(e.target.value)}
+            />
             <label className='form-label'>Nota</label>
-            <input type='text' className='form-control' />
+            <input
+              type='text'
+              className='form-control'
+              onChange={(e) => handleFormNota(e.target.value)}
+            />
 
-            <button className='btn btn-dark'>Agregar tarea</button>
+            <button type='submit' className='btn btn-dark'>
+              Agregar tarea
+            </button>
           </form>
         </div>
       </div>
