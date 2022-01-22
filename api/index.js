90a29693a6a72e72cf7d8ed9d79822a2ad7fd9ea
Daniel Garcia <danvi_@msn.com>
2022-01-22 11:10:24 -0600

    add body parse

--- api/index.js
+++ api/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const DB = [
   { id: 1, nombre: 'Juan', edad: 25 },
@@ -34,6 +39,19 @@ app.get('/usuarios/:id', (req, res) => {
   }
 
   return res.status(404).send();
+});
+
+app.post('/usuarios', (req, res) => {
+  const alta = req.body;
+  const agregar = {
+    id: DB.length + 1,
+    nombre: alta.nombre,
+    edad: alta.edad,
+  };
+
+  DB.push(agregar);
+
+  return res.status(201).json();
 });
 
 app.get('/clientes', (req, res) => {
